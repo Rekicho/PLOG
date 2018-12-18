5b593f4306dc6ae6f516214558a09336969c12c0
Rekicho <up201604145@fe.up.pt>
2018-12-18 13:12:57 +0000

    Circuit

--- TP2/chessloop.pl
+++ TP2/chessloop.pl
@@ -28,49 +28,97 @@ buildPositions(Matrix,Cols,[Head|Tail],TempInd,Indexes,Temp,Positions):-
 buildPositions(Matrix,Cols,[_|Tail],TempInd,Indexes,Temp,Positions):-
 	buildPositions(Matrix,Cols,Tail,TempInd,Indexes,Temp,Positions).
 
-buildAllPositions(_,AllPositions,[],[],AllPositions):-
-	!.
-
-buildAllPositions(Indexes,Positions,[HeadIndex|TailIndex],[HeadPosition|TailPosition],AllPositions):-
-	member(HeadIndex,Indexes),
-	!,
-	append(Indexes,[HeadIndex],NewIndexes),
-	append(Positions,[HeadPosition],NewPositions),
-	buildAllPositions(NewIndexes,NewPositions,TailIndex,TailPosition,AllPositions).
-
-buildAllPositions(Indexes,Positions,[_|TailIndex],[_|TailPosition],AllPositions):-
-	buildAllPositions(Indexes,Positions,TailIndex,TailPosition,AllPositions).
-
-restrict(_,_,_,Line,Lines,_,_):-
+restrict(_,_,_,Line,Lines,_,_,_,_,_):-
 	Line > Lines,
 	!.
 
-restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols):-
+restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 	Col > Cols,
 	!,
 	NextLine is Line + 1,
-	restrict(Matrix,Code1,Code2,NextLine,Lines,1,Cols).
+	restrict(Matrix,Code1,Code2,NextLine,Lines,1,Cols,LoopMatrix,Loop,LoopIndex).
 
-%Checking the one atacked is not the one attacking
-restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols):-
+restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 	Index is ((Line - 1) * Cols) + Col,
 	element(Index,Matrix,Element),
 	attackPositions(Matrix,Code1,Line,Col,Cols,Indexes1,AttackPositions1),
 	attackPositions(Matrix,Code2,Line,Col,Cols,Indexes2,AttackPositions2),
-	buildAllPositions(Indexes1,AttackPositions1,Indexes2,AttackPositions2,AllPositions),
 	count(Code2,AttackPositions1,#=,Attack1),
+	element(LoopIndex1,AttackPositions1,LoopCode2),
+	element(LoopIndex1,Indexes1,Index1),
 	count(Code1,AttackPositions2,#=,Attack2),
+	element(LoopIndex2,AttackPositions2,LoopCode1),
+	element(LoopIndex2,Indexes2,Index2),
+	!,
 	count(Code1,AttackPositions1,#=,Defend1),
 	count(Code2,AttackPositions2,#=,Defend2),
-	count(Code1,AllPositions,#=,All1),
-	count(Code2,AllPositions,#=,All2),
-		Element #= 32 
+	element(Index,LoopMatrix,LoopMatrixElement),
+	element(LoopIndex,Loop,LoopElement),
+		(Element #= 32 #/\ NewLoopIndex #= LoopIndex)
 		#\/ 
-		(Element #= Code1 #/\ Attack1 #= 1 #/\ Defend1 #= 0 #/\ Defend2 #= 1 #/\ All2 #\= 2)
+		(Element #= Code1 #/\ LoopCode2 #= Code2 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index1 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack1 #= 1 #/\ Defend1 #= 0 #/\ Defend2 #= 1)
 		#\/ 
-		(Element #= Code2 #/\ Attack2 #= 1 #/\ Defend2 #= 0 #/\ Defend1 #= 1 #/\ All1 #\= 2),
+		(Element #= Code2 #/\ LoopCode1 #= Code1 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index2 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack2 #= 1 #/\ Defend2 #= 0 #/\ Defend1 #= 1),
 	NextCol is Col + 1,
-	restrict(Matrix,Code1,Code2,Line,Lines,NextCol,Cols).
+	restrict(Matrix,Code1,Code2,Line,Lines,NextCol,Cols,LoopMatrix,Loop,NewLoopIndex).
+
+restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
+	Index is ((Line - 1) * Cols) + Col,
+	element(Index,Matrix,Element),
+	attackPositions(Matrix,Code1,Line,Col,Cols,_,AttackPositions1),
+	attackPositions(Matrix,Code2,Line,Col,Cols,Indexes2,AttackPositions2),
+	count(Code1,AttackPositions2,#=,Attack2),
+	element(LoopIndex2,AttackPositions2,LoopCode1),
+	element(LoopIndex2,Indexes2,Index2),
+	!,
+	count(Code1,AttackPositions1,#=,Defend1),
+	count(Code2,AttackPositions2,#=,Defend2),
+	element(Index,LoopMatrix,LoopMatrixElement),
+	element(LoopIndex,Loop,LoopElement),
+		(Element #= 32 #/\ NewLoopIndex #= LoopIndex)
+		#\/ 
+		(Element #= Code2 #/\ LoopCode1 #= Code1 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index2 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack2 #= 1 #/\ Defend2 #= 0 #/\ Defend1 #= 1),
+	NextCol is Col + 1,
+	restrict(Matrix,Code1,Code2,Line,Lines,NextCol,Cols,LoopMatrix,Loop,NewLoopIndex).
+
+restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
+	Index is ((Line - 1) * Cols) + Col,
+	element(Index,Matrix,Element),
+	attackPositions(Matrix,Code1,Line,Col,Cols,Indexes1,AttackPositions1),
+	attackPositions(Matrix,Code2,Line,Col,Cols,_,AttackPositions2),
+	count(Code2,AttackPositions1,#=,Attack1),
+	element(LoopIndex1,AttackPositions1,LoopCode2),
+	element(LoopIndex1,Indexes1,Index1),
+	!,
+	count(Code1,AttackPositions1,#=,Defend1),
+	count(Code2,AttackPositions2,#=,Defend2),
+	element(Index,LoopMatrix,LoopMatrixElement),
+	element(LoopIndex,Loop,LoopElement),
+		(Element #= 32 #/\ NewLoopIndex #= LoopIndex)
+		#\/ 
+		(Element #= Code1 #/\ LoopCode2 #= Code2 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index1 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack1 #= 1 #/\ Defend1 #= 0 #/\ Defend2 #= 1),
+	NextCol is Col + 1,
+	restrict(Matrix,Code1,Code2,Line,Lines,NextCol,Cols,LoopMatrix,Loop,NewLoopIndex).
+
+restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
+	Index is ((Line - 1) * Cols) + Col,
+	element(Index,Matrix,Element),
+	Element #= 32,
+	NextCol is Col + 1,
+	NewLoopIndex #= LoopIndex + 1,
+	restrict(Matrix,Code1,Code2,Line,Lines,NextCol,Cols,LoopMatrix,Loop,NewLoopIndex).
+
+buildCircuit(_,_,_,Index,Size):-
+	Index > Size,
+	!.
+
+buildCircuit(Circuit,LoopMatrix,Loop,Index,LoopSize):-
+	element(Index,Circuit,Element),
+	element(Index,Loop,LoopElement),
+	element(LoopElement,LoopMatrix,CircuitElement),
+	Element #= CircuitElement,
+	NextIndex is Index + 1,
+	buildCircuit(Circuit,LoopMatrix,Loop,NextIndex,LoopSize).
 
 display_nl(Cols,Cols):-
 	!.
@@ -100,8 +148,8 @@ display_board([Head|Tail],Col,Cols):-
 	Next is Col + 1,
 	display_board(Tail,Next,Cols).
 
-chessloop(Matrix):-
-	puzzle(Num,Piece1,Piece2,Lines,Cols),
+chessloop(ID,Matrix):-
+	puzzle(ID,Num,Piece1,Piece2,Lines,Cols),
 	!,
 	char_code(Piece1,Code1),
 	char_code(Piece2,Code2),
@@ -109,8 +157,14 @@ chessloop(Matrix):-
 	length(Matrix,Size),
 	Max is max(Code1, Code2),
 	domain(Matrix,32,Max),
-	restrict(Matrix,Code1,Code2,1,Lines,1,Cols),
 	count(Code1,Matrix,#=,Num),
 	count(Code2,Matrix,#=,Num),
+	length(LoopMatrix,Size),
+	length(Loop,Size),
+	restrict(Matrix,Code1,Code2,1,Lines,1,Cols,LoopMatrix,Loop,1),
+	CircuitSize is Num * 2,
+	length(Circuit,CircuitSize),
+	buildCircuit(Circuit,LoopMatrix,Loop,1,CircuitSize),
+	circuit(Circuit),
 	labeling([],Matrix),
 	display_board(Matrix,0,Cols).	
