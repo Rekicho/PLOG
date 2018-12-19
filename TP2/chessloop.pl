582b382f9142047412a193b0834e20dc034a652f
Rekicho <up201604145@fe.up.pt>
2018-12-19 18:50:23 +0000

    Faster

--- TP2/chessloop.pl
+++ TP2/chessloop.pl
@@ -49,10 +49,10 @@ subCountCode(_,_,[],CodeCount):-
 
 subCountCode(CountCode,OtherCode,[Head|Tail],CodeCount):-
 	domain([CodeCount],0,1),
-	subCountCode(CountCode,Tail,Temp),
+	subCountCode(CountCode,OtherCode,Tail,Temp),
 	(Head #= CountCode #/\ CodeCount #= 1)
 	#\/
-	(Head #= 32 #/\ CodeCount #= Temp)
+	(Head #= 0 #/\ CodeCount #= Temp)
 	#\/
 	(Head #= OtherCode #/\ CodeCount #= 0).
 
@@ -103,7 +103,7 @@ restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 	!,
 	element(Index,LoopMatrix,LoopMatrixElement),
 	element(LoopIndex,Loop,LoopElement),
-		(Element #= 32 #/\ NewLoopIndex #= LoopIndex)
+		(Element #= 0 #/\ NewLoopIndex #= LoopIndex)
 		#\/ 
 		(Element #= Code1 #/\ LoopCode2 #= Code2 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index1 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack1 #= 1 #/\ Defend1 #= 0 #/\ Defend2 #= 1)
 		#\/ 
@@ -125,7 +125,7 @@ restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 	!,
 	element(Index,LoopMatrix,LoopMatrixElement),
 	element(LoopIndex,Loop,LoopElement),
-		(Element #= 32 #/\ NewLoopIndex #= LoopIndex)
+		(Element #= 0 #/\ NewLoopIndex #= LoopIndex)
 		#\/ 
 		(Element #= Code2 #/\ LoopCode1 #= Code1 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index2 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack2 #= 1 #/\ Defend2 #= 0 #/\ Defend1 #= 1),
 	NextCol is Col + 1,
@@ -145,7 +145,7 @@ restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 	!,
 	element(Index,LoopMatrix,LoopMatrixElement),
 	element(LoopIndex,Loop,LoopElement),
-		(Element #= 32 #/\ NewLoopIndex #= LoopIndex)
+		(Element #= 0 #/\ NewLoopIndex #= LoopIndex)
 		#\/ 
 		(Element #= Code1 #/\ LoopCode2 #= Code2 #/\ LoopMatrixElement #= LoopIndex #/\ LoopElement #= Index1 #/\ NewLoopIndex #= LoopIndex + 1 #/\ Attack1 #= 1 #/\ Defend1 #= 0 #/\ Defend2 #= 1),
 	NextCol is Col + 1,
@@ -154,7 +154,7 @@ restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 restrict(Matrix,Code1,Code2,Line,Lines,Col,Cols,LoopMatrix,Loop,LoopIndex):-
 	Index is ((Line - 1) * Cols) + Col,
 	element(Index,Matrix,Element),
-	Element #= 32,
+	Element #= 0,
 	NextCol is Col + 1,
 	NewLoopIndex #= LoopIndex,
 	restrict(Matrix,Code1,Code2,Line,Lines,NextCol,Cols,LoopMatrix,Loop,NewLoopIndex).
@@ -193,22 +193,21 @@ display_board(Matrix,Cols,Cols):-
 	display_board(Matrix,0,Cols).
 
 display_board([Head|Tail],Col,Cols):-
-	char_code(Piece,Head),
+	pieceCode(Piece,Head),
 	write(Piece),
 	write('|'),
 	Next is Col + 1,
 	display_board(Tail,Next,Cols).
-
-%TODO: CHANGE PIECES VALUES -> FASTER, SEE LABELING OPTIONS
+	
 chessloop(ID,Matrix):-
 	puzzle(ID,Num,Piece1,Piece2,Lines,Cols),
 	!,
-	char_code(Piece1,Code1),
-	char_code(Piece2,Code2),
+	pieceCode(Piece1,Code1),
+	pieceCode(Piece2,Code2),
 	Size is Lines * Cols,
 	length(Matrix,Size),
 	Max is max(Code1, Code2),
-	domain(Matrix,32,Max),
+	domain(Matrix,0,Max),
 	count(Code1,Matrix,#=,Num),
 	count(Code2,Matrix,#=,Num),
 	length(LoopMatrix,Size),
@@ -218,5 +217,5 @@ chessloop(ID,Matrix):-
 	length(Circuit,CircuitSize),
 	buildCircuit(Circuit,LoopMatrix,Loop,1,CircuitSize),
 	circuit(Circuit),
-	labeling([],Matrix),
+	labeling([down],Matrix),
 	display_board(Matrix,0,Cols).	
--- TP2/pieces.pl
+++ TP2/pieces.pl
@@ -1,19 +1,26 @@
-attackPositions(Matrix,107,Line,Col,Cols,Indexes,Positions):-
+pieceCode(' ',0).
+pieceCode(n,1).
+pieceCode(b,2).
+pieceCode(k,3).
+pieceCode(r,4).
+pieceCode(q,5).
+
+attackPositions(Matrix,3,Line,Col,Cols,Indexes,Positions):-
 	!,
 	attackPositionsKing(Matrix,Line,Col,Cols,Indexes,Positions).
 
-attackPositions(Matrix,110,Line,Col,Cols,Indexes,Positions):-
+attackPositions(Matrix,1,Line,Col,Cols,Indexes,Positions):-
 	!,
 	attackPositionsKnight(Matrix,Line,Col,Cols,Indexes,Positions).
 
-attackPositions(Matrix,114,Line,Col,Cols,Indexes,Positions):-
+attackPositions(Matrix,4,Line,Col,Cols,Indexes,Positions):-
 	!,
 	attackPositionsRook(Matrix,Line,Col,Cols,Indexes,Positions).
 
-attackPositions(Matrix,98,Line,Col,Cols,Indexes,Positions):-
+attackPositions(Matrix,2,Line,Col,Cols,Indexes,Positions):-
 	!,
 	attackPositionsBishop(Matrix,Line,Col,Cols,Indexes,Positions).
 
-attackPositions(Matrix,113,Line,Col,Cols,Indexes,Positions):-
+attackPositions(Matrix,5,Line,Col,Cols,Indexes,Positions):-
 	!,
 	attackPositionsQueen(Matrix,Line,Col,Cols,Indexes,Positions).
